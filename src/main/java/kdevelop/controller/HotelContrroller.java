db3c545201b0b5de1e17f37dcee1fac7fe0913d9
MyPC <daflaoui.karim@yahoo.com>
2020-09-27 22:48:04 -0400

    Add save, update and delete methods to hotelController

--- src/main/java/kdevelop/controller/HotelContrroller.java
+++ src/main/java/kdevelop/controller/HotelContrroller.java
@@ -3,10 +3,7 @@ package kdevelop.controller;
 import kdevelop.entities.Hotel;
 import kdevelop.repository.HotelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
@@ -23,5 +20,20 @@ public class HotelContrroller {
     @GetMapping("/all")
     public List<Hotel> getAll(){
         return hotelRepository.findAll();
+    }
+
+    @PutMapping
+    public void insert(@RequestBody  Hotel hotel){
+        this.hotelRepository.insert(hotel);
+    }
+
+    @PostMapping
+    public void update(@RequestBody Hotel hotel){
+        this.hotelRepository.save(hotel);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id){
+        this.hotelRepository.deleteById(id);
     }
 }
